a386827f071aed12bfd12585be5a8fc5fc12a64a
fred <fredphp52@gmail.com>
2026-04-15 15:26:57 +0800

    修改配置

--- public/index.php
+++ public/index.php
@@ -36,8 +36,8 @@ define('app_key','xd5bgugi03uzg9wl');
 define('app_secret','2di85009blxgztyhiuwffu0ogxp8z755');
 define('app_id',232);
 define('baidu_map_key','seTufVuf7Ny0KCPAgRuHbJHOTwunBWOO');
-define('whost','wss://kf5.xywlgzs.vip');
-define('ahost','http://kf5.xywlgzs.vip');
+define('whost','wss://kefu.qdomfk.com');
+define('ahost','https://kefu.qdomfk.com');
 define('wport',443);
 define('aport',2080);
 define('registToken','463907721');
@@ -53,7 +53,7 @@ define('EXTEND_PATH','../extend/');
 define('appid','');
 define('appsecret','');
 define('token','');
-define('domain','http://kf.com');
+define('domain','https://kefu.qdomfk.com');
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
